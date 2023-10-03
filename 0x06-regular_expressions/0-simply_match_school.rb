71e9cc2c6e841e6f65965d28e4de04364986f697
Bright Djogatse <brightdjogatse@gmail.com>
2023-10-03 10:33:39 -0700

    update #!/usr/bin/env ruby puts ARGV[0].scan(/Holberton/).join

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,9 +1,2 @@
 #!/usr/bin/env ruby
-regex = /School/i
-input_command = ARGV[0]
-match = regex.match(input_command)
-if match
-  puts match[0]
-else
-  puts "No match found"
-end
+puts ARGV[0].scan(/Holberton/).join
