77721ce36011601d0980fee4f8d7fea203975f2d
dblay112 <brightdjogatse@gmail.com>
2023-11-24 13:57:18 -0800

    added a comment

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,9 @@
+# to create file in tmp directory
 file { '/tmp/school':
-  ensure => present,
-  owner  => 'www-data',
-  group  => 'www-data',
-  mode   => 0744,
-  content => "I love Puppet\n"
+  ensure  => file,
+  path    => '/tmp/school',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
 }
