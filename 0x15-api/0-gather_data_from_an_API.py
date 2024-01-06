8af8b4cdf9e1621f502a550b54918a60ee08d0ac
dblay112 <brightdjogatse@gmail.com>
2024-01-06 04:44:30 -0800

    gather data file

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -4,3 +4,11 @@ import requests
 import sys
 
 if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
+    todo = requests.get(url + "todo", params={"userId": sys.argv[1]}).json()
+
+    comp = [t.get("title") for i in todo if t.get("completed")is True]
+    print("Employee {} is done with tasks({}/{}):".format(user.get("
+    name"), len(comp), len(todo)))
+    [print("\t {}".format(c)) for c in completed]
