a6f5da431aca6a71a4ce4b9d010c9d0e588111bb
dblay112 <brightdjogatse@gmail.com>
2024-01-16 06:19:22 -0800

    fixed errors

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -3,23 +3,23 @@
  a function that queries the Reddit API and prints the titles of the
  first 10 hot posts listed for a given subreddit
  """
- import requests
+import requests
 
 
- def top_ten(subreddit):
-     """a query the Reddit API and prints the titles
-     of the first 10 hot posts listed for a given subreddit.
-     """
-     url = "https://www.reddit.com/r/{subreddit}/hot.json"
-     headers = {
-             "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/dblay112_)"
-             }
-     response = requests.get(url, headers=headers, params={'limit: 10'}, allow_redirects=False)
-     if response.status_code == 200:
-         data = response.json()
-         results = data["data"]["children"]
+def top_ten(subreddit):
+    """a query to the Reddit API and prints the titles
+    of the first 10 hot posts listed for a given subreddit.
+    """
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/dblay112_)"
+    }
+    response = requests.get(url, headers=headers, params={'limit': 10}, allow_redirects=False)
+    if response.status_code == 200:
+        data = response.json()
+        results = data["data"]["children"]
 
-         for r in results:
-             print(r['data']['title'])
+        for r in results:
+            print(r['data']['title'])
     else:
-        print('None')
+        print('Error:', response.status_code)
