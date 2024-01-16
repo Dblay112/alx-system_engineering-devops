8595842fa2276060c81faa51faae1f2adb0b30a0
Bright Djogatse <125486553+Dblay112@users.noreply.github.com>
2024-01-16 14:10:35 +0000

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,10 +5,10 @@ import requests
 
 def number_of_subscribers(subreddit):
     """returns the total number of subscribers"""
-    url = "https://www.reddit.com/api/v1/subreddits/{subreddit}/about.json"
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"  # Fix the URL
     headers = {
-            "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/dblay112_)"
-            }
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/dblay112_)"
+    }
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 404:
         return 0
