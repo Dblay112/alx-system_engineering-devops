1afcc911e46c30c7bea08d7e441712c0da325f82
Bright Djogatse <125486553+Dblay112@users.noreply.github.com>
2024-01-18 10:18:39 +0000

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,7 +6,7 @@ import requests
 
 def number_of_subscribers(subreddit):
     """returns the total number of subscribers"""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)"
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
         'User-Agent': 'MyChromeBook'
     }
