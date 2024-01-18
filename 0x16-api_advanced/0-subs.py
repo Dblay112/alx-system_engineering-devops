486d9f192308cc7dddcedd967c955ec0820aa175
dblay112 <brightdjogatse@gmail.com>
2024-01-18 05:14:05 -0800

    fixed errors

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,7 +6,7 @@ import requests
 def number_of_subscribers(subreddit):
     """Returns number of subscribers of a subreddit"""
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    headers = {'User-Agent': 'ubuntu/20.04'}
+    headers = {'User-Agent': 'dblay11212'}
     response = requests.get(url, headers=headers, allow_redirects=False)
 
     if response.status_code == 404:
