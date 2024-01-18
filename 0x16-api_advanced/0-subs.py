bdde2d91023d7fba8bb7c595a4bb8048b5fe8bd5
dblay112 <brightdjogatse@gmail.com>
2024-01-18 05:09:34 -0800

    redoing tasks

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python3
-"""module imported"""
+"""funtion to return totoal number"""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Return the total number of subscribers on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {
-        "User-Agent": "MyChromeBook"
-    }
+    """Returns number of subscribers of a subreddit"""
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    headers = {'User-Agent': 'ubuntu/20.04'}
     response = requests.get(url, headers=headers, allow_redirects=False)
+
     if response.status_code == 404:
-        return 0
-    results = response.json().get("data")
-    return results.get("subscribers")
+        return (0)
+    return (response.json().get('data').get('subscribers'))
