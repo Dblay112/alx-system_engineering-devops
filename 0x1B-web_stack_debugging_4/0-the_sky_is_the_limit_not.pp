ac9622acd04deb7d33cd2dcb6a51a84b290a8bbd
dblay112 <brightdjogatse@gmail.com>
2024-01-25 07:38:49 -0800

    fixed errors

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,13 +1,3 @@
-# puppet sctipt to increase the amount of traffic an Nginx server can handle.
-
-# puppet script to increase the ULIMIT of the default file
-exec { 'fix-nginx':
-  command => 'sed -i "s/15/4096/" /etc/default/nginx',
-  path    => '/usr/local/bin/:/bin/'
-} ->
-
-# Restart Nginx
-exec { 'nginx-restart':
-  command => 'nginx restart',
-  path    => '/etc/init.d/'
-}
+# Sky is the limit, let's bring that limit higher
+exec { '/usr/bin/env sed -i s/15/1000/ /etc/default/nginx': }
+-> exec { '/usr/bin/env service nginx restart': }
